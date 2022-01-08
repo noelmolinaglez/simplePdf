673b4eceb5eaade5f14d56940684a9db57613d75
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-07 21:39:03 -0500

    Adding cell dto.

--- pkg/dto/doc.go
+++ pkg/dto/doc.go
@@ -10,10 +10,16 @@ type PDFDoc struct {
 type TitleStruct struct {
 	Title string
 	Font  FontStruct
+	Cell  CellStruct
 }
 
 type FontStruct struct {
 	Family string
 	Style  string
 	Size   float64
+}
+
+type CellStruct struct {
+	Width  float64
+	Height float64
 }
