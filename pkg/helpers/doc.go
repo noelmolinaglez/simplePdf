0d2c02d209fb3bdb507da4b4583d97aa63ddf50f
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-09 13:17:22 -0500

    Subtracting margin size to page width.

--- pkg/helpers/doc.go
+++ pkg/helpers/doc.go
@@ -4,5 +4,7 @@ import "github.com/jung-kurt/gofpdf"
 
 func GetRowWidth(pdf *gofpdf.Fpdf) float64 {
 	width, _ := pdf.GetPageSize()
+	left, _, right, _ := pdf.GetMargins()
+	width = width - (left + right)
 	return width / 12
 }
