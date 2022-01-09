656d3c50d42804d8c31ae0dd83d885f67a1c8ef3
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-08 20:49:35 -0500

    Adding generic table function.

--- pkg/doc/simple.go
+++ pkg/doc/simple.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jung-kurt/gofpdf"
 	"github.com/noelmolinaglez/simplePdf/pkg/dto"
+	"github.com/noelmolinaglez/simplePdf/pkg/interfaces"
 )
 
 //Creates a one page pdf with a title
@@ -25,4 +26,25 @@ func CreateDoc(doc dto.PDFDoc, title dto.TitleStruct) *gofpdf.Fpdf {
 func SaveDoc(pdf *gofpdf.Fpdf, fileName string) error {
 	name := fmt.Sprintf("%s.pdf", fileName)
 	return pdf.OutputFileAndClose(name)
+}
+
+func GenericTable(pdf *gofpdf.Fpdf, data []interfaces.SimpleDoc, table dto.TableStruct) *gofpdf.Fpdf {
+	pdf.SetFont(table.Font.Family, table.Font.Style, table.Font.Size)
+	pdf.SetFillColor(table.Color.R, table.Color.G, table.Color.B)
+
+	headers := data[0].GetHeaders()
+
+	for _, value := range headers {
+		pdf.CellFormat(value.Width, value.Height, value.Content, value.Border, value.Ln, value.Align, value.Fill, value.Link, value.LinkStr)
+	}
+
+	for _, record := range data {
+		for _, value := range record.GetValues() {
+			pdf.CellFormat(value.Width, value.Height, value.Content, value.Border, value.Ln, value.Align, value.Fill, value.Link, value.LinkStr)
+		}
+	}
+
+	pdf.Ln(12)
+
+	return pdf
 }
